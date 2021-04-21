364d5c65058502a3145facb85607ff158d6246a0
zhuyansheng1201 <zhuyansheng1201@gmail.com>
2021-04-21 21:02:34 +0900

    Spring　xmlファイルの活用

--- spring-study/spring-03-ioc2/src/test/java/MyTest.java
+++ spring-study/spring-03-ioc2/src/test/java/MyTest.java
@@ -1,26 +1,24 @@
 import com.zhu.pojo.User;
+import com.zhu.pojo.UserT;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
     public static void main(String[] args) {
-
+        /*
         //Springコンテナ
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
         User user = (User) context.getBean("user");
         User user2 = (User) context.getBean("user");
         user.show();
-        /*
-        IOC　オブジェクトを作成するには
-        デフォルトコンストラクタ！
-        or
-         1.index
-         2.type
-         3.parameters
-        */
+        //IOC　オブジェクトを作成するには デフォルトコンストラクタ！or 1.index 2.type 3.parameters
+
         System.out.println("----------");
         System.out.println(user == user2);
-
+        */
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserT user = (UserT) context.getBean("uu2");
+        user.show();
     }
 }
