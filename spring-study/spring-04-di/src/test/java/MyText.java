01b5a52df1dbf8e16d60a431f4684a95647da38d
zhuyansheng1201 <zhuyansheng1201@gmail.com>
2021-04-24 15:32:21 +0900

    bean scopes

--- spring-study/spring-04-di/src/test/java/MyText.java
+++ spring-study/spring-04-di/src/test/java/MyText.java
@@ -1,12 +1,17 @@
 import com.zhu.pojo.Student;
+import com.zhu.pojo.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyText {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Student student = (Student) context.getBean("student");
-        System.out.println(student.toString());
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbeans.xml");
+        User user = (User) context.getBean("user2",User.class);
+        User user2 = (User) context.getBean("user2",User.class);
+        System.out.println(user.hashCode());
+        System.out.println(user2.hashCode());
+
+        System.out.println(user == user2);
         /*
        Student{
             name='syu ennsei',
